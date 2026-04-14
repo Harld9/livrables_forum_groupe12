668f73c7b4c0f2185bcd2724b20b9193d2ca1b59
Harold <harold.francois@ynov.com>
2026-04-14 11:45:39 +0200

    chore(database): ajout des requêtes afin de créer la base de données

---
+++ app/backend/database/createdb.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE IF NOT EXISTS LoungeDB;
+USE LoungeDB;
+
+CREATE TABLE Utilisateur(
+   idUtilisateur INT AUTO_INCREMENT,
+   pseudo VARCHAR(50),
+   motDePasse VARCHAR(128),
+   dateDeCreation DATE,
+   email VARCHAR(255),
+   typeCompte VARCHAR(50),
+   photoDeProfil VARCHAR(255),
+   Biographie TEXT,
+   derniereConnexion DATETIME,
+   PRIMARY KEY(idUtilisateur)
+);
+
+CREATE TABLE Topic(
+   idTopic INT AUTO_INCREMENT,
+   dateDeCreation DATETIME,
+   titre VARCHAR(50),
+   contenu TEXT,
+   etat VARCHAR(50),
+   idUtilisateur INT NOT NULL,
+   PRIMARY KEY(idTopic),
+   FOREIGN KEY(idUtilisateur) REFERENCES Utilisateur(idUtilisateur)
+);
+
+CREATE TABLE Tag(
+   idTag INT AUTO_INCREMENT,
+   nom VARCHAR(50),
+   PRIMARY KEY(idTag)
+);
+
+CREATE TABLE Message(
+   idMessage INT AUTO_INCREMENT,
+   contenu TEXT,
+   dateEnvoi DATETIME,
+   image VARCHAR(255),
+   idUtilisateur INT NOT NULL,
+   idTopic INT NOT NULL,
+   PRIMARY KEY(idMessage),
+   FOREIGN KEY(idUtilisateur) REFERENCES Utilisateur(idUtilisateur),
+   FOREIGN KEY(idTopic) REFERENCES Topic(idTopic)
+);
+
+CREATE TABLE Appartenir(
+   idTopic INT,
+   idTag INT,
+   PRIMARY KEY(idTopic, idTag),
+   FOREIGN KEY(idTopic) REFERENCES Topic(idTopic),
+   FOREIGN KEY(idTag) REFERENCES Tag(idTag)
+);
+
+CREATE TABLE Evaluer(
+   idUtilisateur INT,
+   idMessage INT,
+   vote SMALLINT,
+   PRIMARY KEY(idUtilisateur, idMessage),
+   FOREIGN KEY(idUtilisateur) REFERENCES Utilisateur(idUtilisateur),
+   FOREIGN KEY(idMessage) REFERENCES Message(idMessage)
+);
