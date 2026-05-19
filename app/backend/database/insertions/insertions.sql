858af82e8dc80cccbb50671a0043517aac9dcf65
Harold <harold.francois@ynov.com>
2026-05-19 11:56:13 +0200

    feat(db): Ajouts de données fictives dans la base de données

---
+++ app/backend/database/insertions/insertions.sql
@@ -0,0 +1,30 @@
+-- Insertions de données fictives dans la table utilisateur
+INSERT INTO
+    client (pseudo, motDePasse, email)
+VALUES
+    ('Michelee', 'root', 'Michelee@gmail.com'),
+    ('Rita', 'root', 'rita@gmail.com'),
+    ('Tetrap', 'root', 'Tetrap@gmail.com'),
+    ('Kabine', 'root', 'Kabinette@gmail.com') ('MOLP', 'root', 'MOLP@gmail.com');
+
+('mollll', 'root', 'Mollll@gmail.com');
+
+-- Insertions de topics fictifs dans la table topic
+INSERT INTO
+    topic (titre, contenu, idUtilisateur)
+VALUES
+    (
+        'Vous en pensez quoi d\'Interstellar ?',
+        'Bonjour, j''écris ce post pour avoir votre avis sur le film Interstellar',
+        7
+    ),
+    (
+        'Vous en pensez quoi d''Inception ?',
+        'Bonjour, j''écris ce post pour avoir votre avis sur le film Inception',
+        8
+    ),
+    (
+        'Vos avis sur l''amour Ouf ?',
+        'Bonjour, j''écris ce poste pour avoir votre avis sur le film l''amour Ouf',
+        9
+    );
