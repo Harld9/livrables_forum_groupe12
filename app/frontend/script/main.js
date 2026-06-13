18d054fbb8c532dbecbfcca893d57d8c8e94bd32
Harold <harold.francois@ynov.com>
2026-06-13 19:32:51 +0200

    style(bouton connexion): Quand l'utilisateur est connecté il en est maintenant informé

--- app/frontend/script/main.js
+++ app/frontend/script/main.js
@@ -1,9 +1,9 @@
 const pseudo = sessionStorage.getItem('pseudo')
+const boutonconnexion = document.getElementById('boutonconnexion')
 
-if (document.getElementById('boutonconnexion')) {
-    const boutonconnexion = document.getElementById('boutonconnexion')
-    if (boutonconnexion && pseudo) {
-        boutonconnexion.remove()
-    }
+if (boutonconnexion && pseudo) {
+    boutonconnexion.textContent = "Bonjour, " + pseudo
+
+    boutonconnexion.classList.add('desactive');
+    boutonconnexion.style.cursor = "default";
 }
-
